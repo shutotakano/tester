b3c12060b839975fadc53cdd9a078a984a33e7bc
shutotakano <lifeguard24242424@gmail.com>
2018-02-13 17:33:13 +0900

    add tuterial

---
+++ tutolial/src/tutorial.cpp
@@ -0,0 +1,39 @@
+#include<ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <turtlesim/Pose.h>
+
+class TurtleSim{
+	public:
+		TurtleSim();
+		void poseCallback(const turtlesim::PoseConstPtr& pose);
+
+	private:
+		//ros::Publisher vel_pub;
+		//ros::Subscriber pose_sub;
+		ros::NodeHandle nh;
+};
+
+TurtleSim::TurtleSim(){
+	vel_pub = nh.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 1000);
+	pose_sub = nh.subscribe<turtlesim::Pose>("turtle1/pose", 1, &TurtleSim::poseCallback, this);
+}
+void TurtleSim::poseCallback(const turtlesim::PoseConstPtr& pose){
+	ROS_INFO("%lf\t%lf",pose->x,pose->y);
+	
+	geometry_msgs::Twist shu;
+	shu.linear.x = 1.0;
+	shu.angular.z = 0.2;
+
+	vel_pub.publish(shu);
+}
+int main(int argc, char **argv){
+	
+	ros::init(argc, argv, "move_turtlesim");
+	TurtleSim turtleSim;
+	
+	ROS_INFO("Hello World");
+	while(ros::ok()){
+		ros::spin();
+	}
+	return 0;
+}
